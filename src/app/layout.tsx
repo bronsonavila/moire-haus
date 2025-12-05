ca8f6a9608fe22dc0f41d2df6d446cb727163ef2
Bronson Avila <bronson.avila@gmail.com>
2025-12-04 16:37:18 -1000

    Enhance metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,47 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+  alternates: { canonical: 'https://moire.haus' },
   authors: [{ name: 'Bronson Avila', url: 'https://www.bronsonavila.com' }],
+  category: 'Art & Design',
   description: 'Moiré wave pattern generator',
   icons: { icon: { url: '/favicon.svg', type: 'image/svg+xml' } },
+  keywords: [
+    'generative art',
+    'interactive art',
+    'moiré pattern',
+    'pattern generator',
+    'visual patterns',
+    'wave generator',
+    'wave interference',
+    'WebGL',
+  ],
+  other: {
+    'github:repo': 'https://github.com/bronsonavila/moire-haus',
+  },
   openGraph: {
     description: 'Moiré wave pattern generator',
+    images: [
+      {
+        alt: 'Moiré Wave Pattern Generator',
+        height: 630,
+        url: 'https://moire.haus/screenshot_1200_630.png',
+        width: 1200,
+      },
+    ],
+    siteName: 'moire.haus',
     title: 'moire.haus',
     type: 'website',
     url: 'https://moire.haus',
   },
+  robots: { index: true, follow: true },
   title: 'moire.haus',
-  twitter: { card: 'summary', description: 'Moiré wave pattern generator', title: 'moire.haus' },
+  twitter: {
+    card: 'summary_large_image',
+    description: 'Moiré wave pattern generator',
+    images: ['https://moire.haus/screenshot_1200_630.png'],
+    title: 'moire.haus',
+  },
 }
 export const viewport: Viewport = {
   initialScale: 1,
